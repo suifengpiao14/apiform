a4bbbafa996cf92fd57486aec987a4a63a69978f
pz <2912150017@qq.com>
2023-02-26 23:12:55 +0800

    optimize

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import MaterialJsonSchemaForm from 'react-jsonschema-form-material-ui';
 
 //import schema from '../simple/schema.json';
-import uiSchema from '../simple/ui-schema.json';
-import givenFormData from '../simple/form-data.json';
+//import uiSchema from '../simple/ui-schema.json';
+//import givenFormData from '../simple/form-data.json';
+import data from '../simple/data.json';
 import { useRouter } from 'next/router';
 import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
@@ -11,14 +12,17 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json())
 export default () => {
   const router = useRouter();
   const { api } = router.query;
-  const { data, error } = useSWR(api, fetcher)
+  //const data={schema:{},example:{},uiSchema:{}}
+  const error=null
+  //const { data, error } = useSWR(api, fetcher)
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
-  const{schema,example}=data
+  const{schema,example,uiSchema}=data
   const [formData, setFormData] = React.useState(example);
   return <MaterialJsonSchemaForm 
             schema={schema}  
             formData={formData} 
+            uiSchema={uiSchema}
             onChange={({ formData }) => setFormData(formData)}
             onSubmit={() => console.log('form submitted')}
           />;
