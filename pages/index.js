797dffdee236c5bbe72c19b613160b2a9d137c7d
zheng peng <pengzheng@huishoubao.com.cn>
2023-03-03 15:58:51 +0800

    optimize

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { withTheme } from '@rjsf/core';
 import Theme from '@rjsf/mui';
 import validator from "@rjsf/validator-ajv8";
 import axios from 'axios';
-import { runBefore,runAfter } from '../lib/functions';
+import { runBefore, runAfter } from '../lib/functions';
 import URL from 'url-parse'
 // Make modifications to the theme with your own fields and widgets
 
@@ -16,7 +16,9 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
+
 export default () => {
+  const { responseData, setResponseData } = useState({})
   const router = useRouter();
   const { api } = router.query;
   //const data={schema:{},example:{},uiSchema:{}}
@@ -26,14 +28,23 @@ export default () => {
   if (!data) return <div>Loading...</div>
   const { schema, example, uiSchema } = data
   const [formData, setFormData] = React.useState(example);
-  return <Form
-    schema={schema}
-    formData={formData}
-    uiSchema={uiSchema}
-    validator={validator}
-    onChange={({ formData }) => setFormData(formData)}
-    onSubmit={({ formData }) => request(formData)}
-  />;
+  return <Fragment>
+    <Form
+      schema={schema}
+      formData={formData}
+      uiSchema={uiSchema}
+      validator={validator}
+      onChange={({ formData }) => setFormData(formData)}
+      onSubmit={({ formData }) => request(formData).then(res => {
+        setResponseData(res.data)
+      })}
+    />
+    <br />
+    <div>
+      返回信息:
+      {JSON.stringify(responseData, " ")}
+    </div>
+  </Fragment>;
 };
 
 
@@ -43,46 +54,39 @@ export default () => {
  * @param {*} formData 
  * @returns 
  */
-
+const proxy = "http://doc-proxy.huishoubao.com"
 function request(formData) {
-  const { service: { method, path, requestPreScript, variables, server:serverName, serverData:servers, requestPostScript }, ...data } = formData
-  const server = filterServer(serverName,servers)
-  data.variables=variables
-  runBefore(requestPreScript,data)
-  const config={
+  const { service: { method, path, requestPreScript, variables, server: serverName, serverData: servers, requestPostScript }, ...data } = formData
+  const server = filterServer(serverName, servers)
+  data.variables = variables
+  runBefore(requestPreScript, data)
+  data.header = data.header || {}
+  const url = new URL(server.host)
+  data.header["X-REAL-HOST"] = url.host
+  if (server.proxy != "") {
+    data.header["X-REAL-ADDRESS"] = server.proxy
+  }
+  const config = {
     url: path,
     method,
-    baseURL:server.host,
-    transformRequest:[
-      function(data,headers){
-      return data
-    }
-  ],
-  headers:data.header,
-  params:data.query,
-  timeout: 30000000,
-  withCredentials:true,
-   }
-   if (server.proxy!=""){
-    debugger
-    const url = new URL(server.proxy);
-    const protocol = url.protocol.replace(":","")
-    config.proxy={
-      protocol:protocol,
-      host:url.host,
-      port:url.port
-    }
-   }
-  axios.request(config).then(res=>{
-    runAfter(requestPostScript,res)
+    baseURL: proxy,
+    headers: data.header,
+    params: data.query,
+    timeout: 30000000,
+    withCredentials: true,
+    data: data.body
+  }
+
+  return axios.request(config).then(res => {
+    runAfter(requestPostScript, res)
+    return res
   })
-  return
 }
 
-function filterServer(name,servers){
-  let server={}
+function filterServer(name, servers) {
+  let server = {}
   servers.forEach(element => {
-    if (element.name==name){
+    if (element.name == name) {
       server = element
     }
   });
